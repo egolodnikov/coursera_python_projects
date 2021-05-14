53a1a16ec0954a6cdddafd7c549a66048dc85f8f
Ernest Golodnikov <eagolodnikov@sberbank.ru>
2021-05-15 04:14:55 +0600

    add test with selenium and fixture

--- tests/test_web.py
+++ tests/test_web.py
@@ -2,8 +2,9 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
 
-url = "https://mozilla.org"
+url = "https://www.mozilla.org"
 title = "Internet for people"
 
 
@@ -11,7 +12,21 @@ title = "Internet for people"
 def web_fix():
     driver = webdriver.Firefox()
     driver.get(url)
+    try:
+        element = wait(driver, 10).until(EC.title_contains(title))
+    except Exception as ex:
+        print(ex)
+    yield driver
+
+    # always quit driver
+    driver.quit()
 
 
 def test_add():
     assert 2 + 3 == 5
+
+
+def test_web_link(web_fix):
+    web_fix.find_element_by_link_text("Загрузить Firefox").click()
+    title = web_fix.title
+    assert 'Firefox' in title
