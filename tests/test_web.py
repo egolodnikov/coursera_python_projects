12b187bfe5ed4ad044dcb16c512dce4514c4f475
Ernest Golodnikov <eagolodnikov@sberbank.ru>
2021-05-15 03:47:38 +0600

    add fixture and requirements

--- tests/test_web.py
+++ tests/test_web.py
@@ -1,2 +1,17 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait as wait
+
+url = "https://mozilla.org"
+title = "Internet for people"
+
+
+@pytest.fixture
+def web_fix():
+    driver = webdriver.Firefox()
+    driver.get(url)
+
+
 def test_add():
     assert 2 + 3 == 5
